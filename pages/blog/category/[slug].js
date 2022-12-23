e5b1e620332aa754b2c226eef4f75867d14383e2
桝田麟太郎 <rinta.barca@gmail.com>
2022-12-23 12:25:02 +0900

    chapter9まで

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,11 +1,18 @@
 import Container from "@/components/container";
+import Meta from "@/components/meta";
 import PostHeader from "@/components/post-header";
-import { getAllCategories } from "lib/api";
+import Posts from "@/components/posts";
+import { getAllCategories, getAllPostsByCategory } from "lib/api";
+import { eyecatchLocal } from "lib/constants";
+import { getPlaiceholder } from "plaiceholder";
 
-export default function Category( { name }) {
+
+export default function Category( { name, posts }) {
   return (
     <Container>
+      <Meta pageTitle={name} pageDesc={`${name}に関する記事`} />
       <PostHeader title={name} subtitle="Blog Category" />
+      <Posts posts={posts} />
     </Container>
   )
 }
@@ -23,9 +30,21 @@ export async function getStaticProps(context) {
 
   const allCats = await getAllCategories()
   const cat = allCats.find(({ slug }) => slug === catSlug)
+
+  const posts = await getAllPostsByCategory(cat.id)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty("eyecatch")) {
+      post.eyecatch = eyecatchLocal;
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    post.eyecatch.blurDataURL = base64;
+  }
+
   return {
     props: {
       name: cat.name,
+      posts: posts,
     },
   };
 }
