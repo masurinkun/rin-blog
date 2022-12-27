a165fd6c3df566692600616260d75b07fcaa72d8
桝田麟太郎 <rinta.barca@gmail.com>
2022-12-27 12:59:38 +0900

    chapter10.5まで

---
+++ components/accordion.js
@@ -0,0 +1,34 @@
+import styles from "styles/accordion.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { useRef, useState } from "react";
+
+export default function Accordion({ heading, children }) {
+  const [ textIsOpen, setTextIsOpen ] = useState(false)
+
+  const toggleText = () => {
+    setTextIsOpen((prev) => !prev)
+  }
+
+  const refText = useRef( {scrollHeight: 0 })
+
+  return (
+    <div className={textIsOpen? styles.open : styles.close}>
+      <h3 className={styles.heading}>
+        <button onClick={toggleText}>
+          {heading}
+          <FontAwesomeIcon icon={faCircleChevronDown} className={styles.icon} />
+        </button>
+      </h3>
+      <div
+        className={styles.text}
+        ref={refText}
+        style={{
+          '--text-height': `${refText.current.scrollHeight}px`,
+        }}
+      >
+        <div className={styles.textInner}>{children}</div>
+      </div>
+    </div>
+  )
+}
--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,51 @@
 import Link from "next/link";
+import { useState } from "react";
 import styles from "styles/nav.module.scss";
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
+
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
+
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {/*menuが開いた時にページがスクロールしないようにする*/}
+      {navIsOpen && (
+        <style jsx global>{`
+          @media (max-width: 767px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          }
+        `}
+        </style>
+      )}
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span className="sr-only">MENU</span>
+      </button>
       <ul className={styles.list}>
         <li>
           <Link href="/">
-            <span>Home</span>
+            <span onClick={closeNav}>Home</span>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <span>About</span>
+            <span onClick={closeNav}>About</span>
           </Link>
         </li>
         <li>
           <Link href="/blog">
-            <span>Blog</span>
+            <span onClick={closeNav}>Blog</span>
           </Link>
         </li>
       </ul>
